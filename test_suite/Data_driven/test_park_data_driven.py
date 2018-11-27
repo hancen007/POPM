aa319ab8ff00de3e9195e7917cc3d39dd3d6fb49
岑苏岸 <suan1.cen@partner.midea.com>
2018-11-27 10:45:07 +0800

    添加登陆

--- test_suite/Data_driven/test_park_data_driven.py
+++ test_suite/Data_driven/test_park_data_driven.py
@@ -12,17 +12,15 @@ root_path = os.path.abspath(os.path.join(BASE_DIR, "../.."))
 from common.utils import YmlUtils
 from Api.parkVisitorlist import parkVistorlist
 
-args_item = "send_data,expect"
 
+args_item = "send_data,expect"
 test_data,case_desc = YmlUtils("/test_data/Park/parklist.yml").getData
-@allure.feature("博客文章内容")
 @pytest.mark.parametrize(args_item, test_data, ids=case_desc)
 class TestWordpress():
     """验证搜索的关键字"""
     @allure.story('验证查询')
-    def test_getlist(self,send_data,expect):
-
-        re = parkVistorlist().getParklist(send_data["carLicenseNumber"])
+    def test_getlist(self,userLogin,send_data,expect):
+        re = parkVistorlist(userLogin).getParklist(send_data["carLicenseNumber"])
         result =re.json()
         assert result["status"] == expect["status"]
         assert result["resultCode"] == expect["resultCode"]
